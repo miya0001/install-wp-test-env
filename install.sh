e02e9b15e3ca6ef624612e5288c4aa137a4391d9
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2017-11-02 03:45:36 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 #SKIP_DB_CREATE=${6-false}
 
+TMPDIR=${TMPDIR-/tmp}
+TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
+WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
+
 # set DB_PASS with -p argument to send it mysql command if set DB_PASS
 if [ -z "${DB_PASS}" ]; then
   # "empty" might be connect by socket
@@ -20,12 +25,12 @@ set -ex;
 
 echo 'DROP DATABASE IF EXISTS '${DB_NAME}';' | mysql -u ${DB_USER} -h ${DB_HOST} ${DB_PASS_ARG}
 
-if [ -e /tmp/wordpress ]; then
-    rm -fr /tmp/wordpress
+if [ -e $WP_CORE_DIR ]; then
+    rm -fr $WP_CORE_DIR
 fi
 
-if [ -e /tmp/wordpress-tests-lib ]; then
-    rm -fr /tmp/wordpress-tests-lib
+if [ -e $WP_TESTS_DIR ]; then
+    rm -fr $WP_TESTS_DIR
 fi
 
 bash bin/install-wp-tests.sh "${DB_NAME}" "${DB_USER}" "${DB_PASS}" "${DB_HOST}" "${WP_VERSION}";
